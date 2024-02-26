bb5ced8990cad6608e10d091e5b1325502ae5674
lennertnijs <lennert.nijs2001@icloud.com>
2024-02-27 00:18:27 +0100

    Moving around classes into proper packages & adding hashCode methods

--- src/java/com/Textr/Inputs.java
+++ src/java/com/Textr/Inputs.java
@@ -8,7 +8,13 @@ public class Inputs {
     public static final int CTRL_R = 18;
     public static final int CTRL_S = 19;
     public static final int CTRL_T = 20;
+    public static final int ESCAPE = 27;
+    public static final int ARROW_UP = 65;
+    public static final int ARROW_DOWN = 66;
+    public static final int ARROW_RIGHT = 67;
+    public static final int ARROW_LEFT = 68;
     public static final int N_UPPER = 78;
+    public static final int F4 = 83;
     public static final int Y_UPPER = 89;
     public static final int N_LOWER = 110;
     public static final int Y_LOWER = 121;
