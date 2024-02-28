e1349181813a7e343db7ae32214f89dcaf1840e5
lennertnijs <lennert.nijs2001@icloud.com>
2024-02-28 19:18:04 +0100

    doing work in the FileBufferRepository.java

--- src/java/com/Textr/Inputs.java
+++ src/java/com/Textr/Inputs.java
@@ -2,6 +2,7 @@ package com.Textr;
 
 public class Inputs {
 
+    public static final int BACKSPACE = 8;
     public static final int RETURN = 13;
     public static final int CTRL_N = 14;
     public static final int CTRL_P = 16;
@@ -9,6 +10,7 @@ public class Inputs {
     public static final int CTRL_S = 19;
     public static final int CTRL_T = 20;
     public static final int ESCAPE = 27;
+    public static final int SPACE = 32;
     // THE ARROW KEYS ARE AN ESCAPE KEY COMBINATION, AKA ESCAPE + [ + THE CODE HERE.
     public static final int ARROW_UP = 65;
     public static final int ARROW_DOWN = 66;
@@ -19,5 +21,4 @@ public class Inputs {
     public static final int Y_UPPER = 89;
     public static final int N_LOWER = 110;
     public static final int Y_LOWER = 121;
-    public static final int BACKSPACE = 127;
 }
