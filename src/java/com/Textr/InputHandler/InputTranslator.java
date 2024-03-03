ecc14c24a3dd577dc2abf15c3143786b342414be
lennertnijs <lennert.nijs2001@icloud.com>
2024-03-03 14:50:17 +0100

    doing work in the FileBufferRepository.java

--- src/java/com/Textr/InputHandler/InputTranslator.java
+++ src/java/com/Textr/InputHandler/InputTranslator.java
@@ -6,40 +6,73 @@ import java.util.InputMismatchException;
 
 import static com.Textr.InputHandler.Inputs.*;
 
+/**
+ * Used to translate a byte or stream of bytes into single input.
+ */
 public class InputTranslator {
 
+    /**
+     * Private constructor. No use.
+     */
     private InputTranslator(){
     }
 
+    /**
+     * Translates the byte or bytes from the input into a single {@link Input}.
+     * @param b The first byte.
+     *
+     * @return The {@link Input} enum representing an input.
+     * @throws InputMismatchException If no translation was found for the given input byte(s).
+     */
     public static Input translateBytes(int b){
-        switch(b){
-            case ENTER:
+        switch (b) {
+            case ENTER -> {
                 return Input.ENTER;
-            case CTRL_N:
+            }
+            case CTRL_N -> {
                 return Input.CTRL_N;
-            case CTRL_P:
+            }
+            case CTRL_P -> {
                 return Input.CTRL_P;
-            case SPACE:
+            }
+            case SPACE -> {
                 return Input.SPACE;
-            case BACKSPACE:
+            }
+            case BACKSPACE -> {
                 return Input.BACKSPACE;
-            case ESCAPE:
+            }
+            case ESCAPE -> {
                 return translateEscapeByteCode();
+            }
         }
         throw new InputMismatchException("Could not translate the input.");
     }
 
+    /**
+     * Translates the second byte of an escape sequence. (The first character was ESCAPE).
+     *
+     * @return The translated {@link Input}
+     * @throws InputMismatchException If no translation was found for the given second input byte.
+     */
     private static Input translateEscapeByteCode(){
         int b = TerminalService.readByte();
-        switch(b){
-            case '[':
+        switch (b) {
+            case '[' -> {
                 return translateBracketByteCode();
-            case 'O':
+            }
+            case 'O' -> {
                 return translateOByteCode();
+            }
         }
         throw new InputMismatchException("Could not translate the input.");
     }
 
+    /**
+     * Translates the third byte of an escape sequence, if the second was the right bracket. ([)
+     *
+     * @return The translated {@link Input}.
+     * @throws InputMismatchException If no translation was found for the given third input byte.
+     */
     private static Input translateBracketByteCode(){
         int b = TerminalService.readByte();
         switch(b){
@@ -62,6 +95,27 @@ public class InputTranslator {
         throw new InputMismatchException("Could not translate the input.");
     }
 
+
+    /**
+     * Translates the third byte of an escape sequence, if the second was the capital O.
+     *
+     * @return The translated {@link Input}.
+     * @throws InputMismatchException If no translation was found for the given third input byte.
+     */
+    private static Input translateOByteCode(){
+        int b = TerminalService.readByte();
+        if(b == 'S'){
+            return Input.F4;
+        }
+        throw new InputMismatchException("Could not translate the input.");
+    }
+
+    /**
+     * Translates the fourth byte of an escape sequence, if the third was the number 3.
+     *
+     * @return The translated {@link Input}.
+     * @throws InputMismatchException If no translation was found for the given fourth input byte.
+     */
     private static Input translate3ByteCode(){
         int b = TerminalService.readByte();
         if(b == '~'){
@@ -70,11 +124,4 @@ public class InputTranslator {
         throw new InputMismatchException("Could not translate the input.");
     }
 
-    private static Input translateOByteCode(){
-        int b = TerminalService.readByte();
-        if(b == 'S'){
-            return Input.F4;
-        }
-        throw new InputMismatchException("Could not translate the input.");
-    }
 }
