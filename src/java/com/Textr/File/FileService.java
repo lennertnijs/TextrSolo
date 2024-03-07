ca3cd39f9dcd3c88fcce441245fe2db9c2baf18e
lennertnijs <lennert.nijs2001@icloud.com>
2024-03-08 00:53:03 +0100

    Moving around files

--- src/java/com/Textr/File/FileService.java
+++ src/java/com/Textr/File/FileService.java
@@ -42,19 +42,14 @@ public final class FileService {
     /**
      * Saves the given data to disk, at the location specified by the File with given fileID.
      *
-     * @param data    The String to write to disk
-     * @param fileID  The relevant fileID to write to.
+     * @param text    The String to write to disk
+     * @param fileId  The relevant fileId to write to.
      * @throws IOException when file could not be opened or created, or something went wrong during writing.
      */
-    public void saveToFile(String data, int fileID) throws IOException {
-        File file = getFile(fileID);
-        String bufferUrl = file.getUrl();
-
-        // Possible IOException:
-        FileWriter.writeToFile(data, bufferUrl);
-
-        // If this code is reached, writing was successful: update File text
-        file.setText(data);
+    public void saveToFile(String text, int fileId) throws IOException {
+        File file = getFile(fileId);
+        FileWriter.write(text, file.getUrl());
+        file.setText(text);
     }
 
     /**
