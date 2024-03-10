e24053fa5b5aad40afd270f9ae304fc548794f1c
lennertnijs <lennert.nijs2001@icloud.com>
2024-03-10 13:16:24 +0100

    Cleaning up code

--- src/test/FileTest/FileServiceTest.java
+++ src/test/FileTest/FileServiceTest.java
@@ -1,4 +1,26 @@
 package FileTest;
 
+import com.Textr.File.FileRepo;
+import com.Textr.File.FileService;
+import com.Textr.File.IFileRepo;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class FileServiceTest {
+
+
+    private IFileRepo repo;
+    private FileService fileService;
+
+
+    @BeforeEach
+    public void initialise(){
+        repo = new FileRepo();
+        fileService = new FileService(repo);
+    }
+
+    @Test
+    public void testAll(){
+
+    }
 }
