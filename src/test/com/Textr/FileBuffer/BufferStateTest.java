44a6f24a782ff41fae09050242920a6c983b9e3b
Dieter Demuynck <dieter.demuynck@student.kuleuven.be>
2024-03-11 15:28:22 +0100

    BufferState "test"

---
+++ src/test/com/Textr/FileBuffer/BufferStateTest.java
@@ -0,0 +1,18 @@
+package com.Textr.FileBuffer;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BufferStateTest {
+
+    // The main reason for this test case is to boost coverage.
+
+    @Test
+    void valueOf() {
+        assertEquals(BufferState.CLEAN, BufferState.valueOf("CLEAN"));
+        assertEquals(BufferState.DIRTY, BufferState.valueOf("DIRTY"));
+    }
+}
