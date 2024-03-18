804a420a078c7a9d52ee34449b37b54e90477e06
simon <simon.andriessen2@gmail.com>
2024-03-18 15:10:01 +0100

    BUGFIX LayoutGenerator

--- src/java/com/Textr/View/LayoutGenerator.java
+++ src/java/com/Textr/View/LayoutGenerator.java
@@ -85,44 +85,4 @@ public static void generateLayoutsHorizontalSubTree(Point topLeft, Point bottomR
         x += LayoutWidth;
     }
 }
-
-//    private static void generateVerticallyStackedViews(Point topLeft, Dimension2D dimensions, int depth){
-//        List<View> nodeCount = viewRepo.getAllAtDepth(depth);
-//        int amountOfViews = nodeCount.size();
-//        int heightPerView = dimensions.getHeight() / amountOfViews;
-//        int remainder = dimensions.getHeight() % amountOfViews;
-//        int y = topLeft.getY();
-//        for(View view : nodeCount){
-//            Point position = Point.create(topLeft.getX(), y);
-//            int viewHeight = remainder-- > 0 ? heightPerView + 1 : heightPerView;
-//            Dimension2D dimensionsOfView = Dimension2D.create(dimensions.getWidth(), viewHeight);
-//            if(view == null){
-//                generateHorizontallyStackedViews(position, dimensionsOfView, depth + 1);
-//            }else{
-//                view.setPosition(position);
-//                view.setDimensions(dimensionsOfView);
-//            }
-//            y += viewHeight;
-//        }
-//    }
-//
-//    private static void generateHorizontallyStackedViews(Point topLeft, Dimension2D dimensions, int depth){
-//        List<View> nodeCount = viewRepo.getAllAtDepth(depth);
-//        int amountOfViews = nodeCount.size();
-//        int widthPerView = dimensions.getWidth() / amountOfViews;
-//        int remainder = dimensions.getWidth() % amountOfViews;
-//        int x = topLeft.getX();
-//        for(View view : nodeCount){
-//            Point position = Point.create(x, topLeft.getY());
-//            int viewWidth = remainder-- > 0 ? widthPerView + 1 : widthPerView;
-//            Dimension2D dimensionsOfView = Dimension2D.create(viewWidth, dimensions.getHeight());
-//            if(view == null){
-//                generateVerticallyStackedViews(position, dimensionsOfView, depth + 1);
-//            }else{
-//                view.setPosition(position);
-//                view.setDimensions(dimensionsOfView);
-//            }
-//            x += viewWidth;
-//        }
-//    }
 }
