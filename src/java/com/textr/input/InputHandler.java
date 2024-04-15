0d2c6e9b7e3503cab9bc00d426bea023fe937dbc
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-15 15:32:10 +0200

    Added a bunch of things. Cursor does not yet move, so odd behaviour

--- src/java/com/textr/input/InputHandler.java
+++ src/java/com/textr/input/InputHandler.java
@@ -1,9 +1,6 @@
 package com.textr.input;
 
-import com.textr.util.Direction;
 import com.textr.view.ViewService;
-
-import java.util.Objects;
 
 public final class InputHandler implements IInputHandler {
     private final ViewService viewService;
@@ -15,7 +12,6 @@ public final class InputHandler implements IInputHandler {
     @Override
     public void handleInput(){
         Input input = InputTranslator.getNextInput();
-        InputType inputType = input.getType();
         viewService.handleInput(input);
     }
 
