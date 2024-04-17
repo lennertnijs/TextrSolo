6df089a20317ecb0ca35095f02aca29f6090de34
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-17 11:56:43 +0200

    Added FoodManager

---
+++ src/java/com/textr/view/FoodManager.java
@@ -0,0 +1,37 @@
+package com.textr.view;
+
+import com.textr.util.Point;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public final class FoodManager {
+
+    private final List<Point> foods;
+
+    public FoodManager(){
+        foods = new ArrayList<>();
+    }
+
+    public List<Point> getFoods(){
+        return foods;
+    }
+
+    public boolean isFood(Point p){
+        Objects.requireNonNull(p, "Point is null.");
+        return foods.stream().anyMatch(food -> food.equals(p));
+    }
+
+    public void add(Point p){
+        Objects.requireNonNull(p, "Point is null.");
+        if(foods.stream().anyMatch(food -> food.equals(p)))
+            throw new IllegalStateException("Duplicate Food detected.");
+        foods.add(p);
+    }
+
+    public void remove(Point p){
+        Objects.requireNonNull(p, "Point is null.");
+        foods.remove(p);
+    }
+}
