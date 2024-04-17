86d96db60bd306aa66f38add3ddbe9c60dd4e072
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-17 17:56:07 +0200

    FoodManager javadoc

--- src/java/com/textr/snake/FoodManager.java
+++ src/java/com/textr/snake/FoodManager.java
@@ -1,43 +1,73 @@
 package com.textr.snake;
 
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.NoSuchElementException;
 import java.util.Objects;
 
 public final class FoodManager {
 
+    /**
+     * List of {@link GamePoint}s representing the locations of food in the game.
+     */
     private final List<GamePoint> foods;
 
+    /**
+     * Initializes a new {@link FoodManager} with no foods.
+     */
     public FoodManager(){
-        foods = new ArrayList<>();
+        foods = new LinkedList<>();
     }
 
+    /**
+     * @return The amount of foods.
+     */
     public int getFoodCount(){
         return foods.size();
     }
+
+    /**
+     * @return A copy of the List of foods.
+     */
     public List<GamePoint> getFoods(){
-        return foods;
+        return new LinkedList<>(foods);
     }
 
+    /**
+     * Checks whether the given {@link GamePoint} holds food.
+     * @param p The {@link GamePoint}. Cannot be null.
+     *
+     * @return True if it holds food. False otherwise.
+     */
     public boolean isFood(GamePoint p){
-        Objects.requireNonNull(p, "Point is null.");
+        Objects.requireNonNull(p, "GamePoint is null.");
         return foods.stream().anyMatch(food -> food.equals(p));
     }
 
+    /**
+     * Adds the given {@link GamePoint} to the List of foods.
+     * @param p The {@link GamePoint}. Cannot be null.
+     *
+     * @throws IllegalStateException If the given {@link GamePoint} already held food.
+     */
     public void add(GamePoint p){
-        Objects.requireNonNull(p, "Point is null.");
+        Objects.requireNonNull(p, "GamePoint is null.");
         if(foods.stream().anyMatch(food -> food.equals(p)))
-            throw new IllegalStateException("Duplicate Food detected.");
+            throw new IllegalStateException("Duplicate food detected.");
         foods.add(p);
     }
 
-
+    /**
+     * Removes the {@link GamePoint} if it held a piece of food.
+     * @param p The {@link GamePoint}. Cannot be null.
+     */
     public void remove(GamePoint p){
-        Objects.requireNonNull(p, "Point is null.");
+        Objects.requireNonNull(p, "GamePoint is null.");
         foods.remove(p);
     }
 
+    /**
+     * Removes all foods.
+     */
     public void clearFoods(){
         foods.clear();
     }
