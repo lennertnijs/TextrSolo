1a93a9518ca6a2e4f3bb6ebf59ae3540d582e497
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-17 13:40:14 +0200

    Added clock

---
+++ src/java/com/textr/snake/Clock.java
@@ -0,0 +1,25 @@
+package com.textr.snake;
+
+public final class Clock {
+
+    private float lastMove;
+    private float timeBetweenMoves;
+
+    public Clock(float timeBetweenMoves){
+        lastMove = System.currentTimeMillis();
+        this.timeBetweenMoves = timeBetweenMoves;
+    }
+
+
+    public boolean shouldMove(){
+        return System.currentTimeMillis() - lastMove > timeBetweenMoves;
+    }
+
+    public void reset(){
+        lastMove = System.currentTimeMillis();
+    }
+
+    public void updateTimeBetweenMoves(){
+        timeBetweenMoves *= 0.8;
+    }
+}
