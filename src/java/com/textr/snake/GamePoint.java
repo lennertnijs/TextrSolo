277a081421aa46c65529b8b8c65cb61046ce0bd6
lennertnijs <lennert.nijs2001@icloud.com>
2024-04-17 14:24:12 +0200

    Tested GamePoint. Documented GamePoint. Also made a Record out of it.

--- src/java/com/textr/snake/GamePoint.java
+++ src/java/com/textr/snake/GamePoint.java
@@ -2,20 +2,39 @@ package com.textr.snake;
 
 import java.util.Objects;
 
+/**
+ * Represents a 2-dimensional location in a game context.
+ * @param x The x coordinate. Can be a negative value.
+ * @param y The y coordinate. Can be a negative value.
+ */
 public record GamePoint(int x, int y) {
 
+    /**
+     * Adds the given {@link Vector} to this {@link GamePoint} and returns the result.
+     * Does not change the original {@link GamePoint}.
+     * @param vector The translation {@link Vector}. Cannot be null.
+     *
+     * @return The translated {@link GamePoint}.
+     */
     public GamePoint translate(Vector vector) {
         Objects.requireNonNull(vector, "Vector is null.");
         return new GamePoint(x + vector.getX(), y + vector.getY());
     }
 
+    /**
+     * Compares this {@link GamePoint} to the given {@link Object}.
+     * Returns True if they're equal. False otherwise.
+     */
     @Override
     public boolean equals(Object other) {
-        if (!(other instanceof GamePoint p))
+        if (!(other instanceof GamePoint gamePoint))
             return false;
-        return x == p.x && y == p.y;
+        return x == gamePoint.x && y == gamePoint.y;
     }
 
+    /**
+     * Creates a {@link String} representation of this {@link GamePoint} and returns it.
+     */
     @Override
     public String toString() {
         return String.format("GamePoint[x=%d, y=%d]", x, y);
