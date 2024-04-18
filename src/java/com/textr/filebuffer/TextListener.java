49f141a1e4d1ad2f7891b54c732b182e4ee9e1b7
Dieter Demuynck <dieter.demuynck@student.kuleuven.be>
2024-04-18 02:15:59 +0200

    Removed unused imports

--- src/java/com/textr/filebuffer/TextListener.java
+++ src/java/com/textr/filebuffer/TextListener.java
@@ -1,8 +1,5 @@
 package com.textr.filebuffer;
 
-import com.textr.util.FixedPoint;
-import com.textr.util.Point;
-
 public interface TextListener {
     void update(TextUpdateReference update, ITextSkeleton structure);
 }
